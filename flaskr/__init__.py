14ea1335e1d737cf26e725bb30026e8dae43be17
Michael Uloth <hello@michaeluloth.com>
2022-06-11 14:04:13 -0400

    add blog index

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -35,7 +35,7 @@ def create_app(test_config=None):
 	app.register_blueprint(auth.bp)
 
 	from . import blog
-  app.register_blueprint(blog.bp)
-  app.add_url_rule('/', endpoint='index')
-	
+	app.register_blueprint(blog.bp)
+	app.add_url_rule('/', endpoint='index')
+
 	return app
