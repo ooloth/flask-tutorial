b7e2f62c557cd1974f77114735c05460d83ea8fb
Michael Uloth <hello@michaeluloth.com>
2022-06-11 16:07:17 -0400

    test auth logic

--- tests/conftest.py
+++ tests/conftest.py
@@ -35,3 +35,21 @@ def client(app):
 def runner(app):
     # The runner fixture is similar to client. app.test_cli_runner() creates a runner that can call the Click commands registered with the application.
     return app.test_cli_runner()
+
+# For most of the views, a user needs to be logged in. The easiest way to do this in tests is to make a POST request to the login view with the client. Rather than writing that out every time, you can write a class with methods to do that, and use a fixture to pass it the client for each test.
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='test', password='test'):
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
