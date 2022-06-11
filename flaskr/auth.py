55b9299198fe8c8e5f45702985e763506a42f689
Michael Uloth <hello@michaeluloth.com>
2022-06-11 13:24:17 -0400

    create "register" view logic

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -8,3 +8,34 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+  # If the request method is POST, validate the input + register the user
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		db = get_db()
+		error = None
+
+		if not username:
+			error = 'Username is required.'
+		elif not password:
+			error = 'Password is required.'
+
+		if error is None:
+			try:
+				db.execute(
+					"INSERT INTO user (username, password) VALUES (?, ?)",
+					(username, generate_password_hash(password)),
+				)
+				db.commit()
+			except db.IntegrityError:
+				error = f"User {username} is already registered."
+			else:
+				return redirect(url_for("auth.login"))
+
+		flash(error)
+
+  # If the request method is GET, render the register template
+	return render_template('auth/register.html')
